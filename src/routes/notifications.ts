996a9ff887cecfe51b75d86936dce4b232275243
AsharMeraj <ezshifakiosk37@gmail.com>
2026-04-30 17:12:29 +0500

    Added Call Audit

--- src/routes/notifications.ts
+++ src/routes/notifications.ts
@@ -180,6 +180,7 @@ router.get('/call-status/:vitalsId', authenticate, async (req: Request, res: Res
 // POST /api/notifications/end-call
 router.post('/end-call', authenticate, async (req: any, res: Response) => {
   const { vitalsId, reason } = req.body; // Add 'reason' here
+  console.log(reason)
 
   if (!vitalsId) {
     return res.status(400).json({ error: "Vitals ID required" });
